33d697bb50254c5def46d41dce2984019e9c5f53
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-10 17:52:25 -0600

    added task handler function event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,23 @@
-var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-
-var buttonEl = document.querySelector("#save-task");
+// Whole form event listener
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Creating a task item function
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-};
+  };
+  
+ 
 
 // Event Listener
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
+
+
 
 
 
