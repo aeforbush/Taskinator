75364bf10c5c3c116914f65757a39421e66606c5
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-11 17:22:38 -0600

    addressed usability concerns

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,19 @@ var taskFormHandler = function(event) {
     // Find value for task type
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    // Check if input values are empty strings
+    if (taskNameInput === "" || taskTypeInput === "") {
+      alert("You need to fill out the task form!");
+      return false;
+    }
+    
+    // Reset form
+    formEl.reset();
+
+    // Reset form fields for next task to be entered
+    document.querySelector("input[name='task-name']").value = "";
+    document.querySelector("select[name='task-type']").selectedIndex = 0;
+
     // Package up data as an object
     var taskDataObj = {
         name: taskNameInput,
@@ -18,17 +31,14 @@ var taskFormHandler = function(event) {
 
     // send it as an argument to createTaskEl
     createTaskEl(taskDataObj);
-
-
-   
   };
   // Function to accept the form values as arguments and use them to create the new task item's HTML
   var createTaskEl = function(taskDataObj) {
-          // Create list item
+    // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
-    // Createa div to hold task info and add to list item
+    // Create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     // Give it a class name
     taskInfoEl.className = "task-info";
@@ -38,7 +48,6 @@ var taskFormHandler = function(event) {
 
     // Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-
   };
 
 // Event Listener
