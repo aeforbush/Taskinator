622391212b82c58b621c5a6e50ca418bfbef6a24
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-08-10 18:52:42 -0600

    created a task form handler function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,14 +3,28 @@ var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // Creating a task item function
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     // Find value for task name
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     // Find value for task type
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    // Create list item
+    // Package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+
+
+   
+  };
+  // Function to accept the form values as arguments and use them to create the new task item's HTML
+  var createTaskEl = function(taskDataObj) {
+          // Create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
@@ -25,16 +39,18 @@ var createTaskHandler = function(event) {
 
     // Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-    console.dir(listItemEl);
-  };
+
+  }
 
 // Event Listener
-formEl.addEventListener("submit", createTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
 
 // Ability to read and store users input
 
 
 
+
+// One function to handle the form submission, get the form values, and pass those values to another function as arguments
 
 
 
